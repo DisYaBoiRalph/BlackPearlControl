491e9d5131562d85b44ce9fd741f3e1ff5c4781c
cheesyserg <darylgenemata7@gmail.com>
2026-06-05 11:54:04 -0700

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.chesaudio.bpcontrol"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.4"
+        versionCode = 5
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
