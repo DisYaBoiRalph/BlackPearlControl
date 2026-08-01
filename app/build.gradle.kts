ffa2b4bf305cb7cb64fb71b20091a149ec68a5c8
disyaboiralph <danque62@gmail.com>
2026-08-01 10:03:55 +0800

    chore(release): bump version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.fossyaudio.bpcontrol"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.4"
+        versionCode = 5
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
